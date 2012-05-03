8eccf8c16a0d094cdfb23485f3dcbd6abd9f378c
Daniel W. Steinbrook <dws@elys.com>
2012-05-03 13:05:36 -0400

    reverting test1

--- test/test1.rb
+++ test/test1.rb
@@ -4,11 +4,19 @@ require '../fluidsynth.rb'
 fs = FluidSynth.new
 fs.start
 
-sfid = fs.sfload("/Users/jj/Downloads/FluidR3_GM.sf2",1)
-#fs.program_select(0, sfid, 0, 0)
+sfid = fs.sfload("example.sf2")
+fs.program_select(0, sfid, 0, 0)
 
-fs.play_file("/Users/jj/Desktop/badboys_xg.mid")
+fs.noteon(0, 60, 30)
+fs.noteon(0, 67, 30)
+fs.noteon(0, 76, 30)
 
-sleep 30
+sleep(1.0)
+
+fs.noteoff(0, 60)
+fs.noteoff(0, 67)
+fs.noteoff(0, 76)
+
+sleep(1.0)
 
 fs.delete
